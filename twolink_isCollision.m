1a389f3f9b604a0a023d7c24a1e1c98c0c7dce8d
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-10-05 05:14:15 -0400

    Fixed collision check

--- twolink_isCollision.m
+++ twolink_isCollision.m
@@ -4,9 +4,10 @@
 %function polygon_isCollision to check if each link of the manipulator is in
 %collision.
 function [flagTheta]=twolink_isCollision(theta,points)
-    [~,vertices1,vertices2] = twolink_kinematicMap(theta);
-    twolink_plot(theta,'r',[0;0])
-    flagTheta = bitor(polygon_isCollision(vertices1,points), polygon_isCollision(vertices2,points));
+    for i=1:size(theta,2)
+        [~,vertices1,vertices2] = twolink_kinematicMap(theta(:,i);
+        twolink_plot(theta(:,i),'r',[0;0])
+        flagTheta = any([any(polygon_isCollision(vertices1,points)) any(polygon_isCollision(vertices2,points))]);
 end
 %For this question,  do not consider self-collision (i.e., if the two polygons
 %overlap but they do not cover any of the points, then it is not a collision).

031524a85c2baefb6b5fd82ab490dcafb68cad10
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-10-05 05:19:24 -0400

    Update twolink_isCollision.m

--- twolink_isCollision.m
+++ twolink_isCollision.m
@@ -7,7 +7,7 @@ function [flagTheta]=twolink_isCollision(theta,points)
     for i=1:size(theta,2)
         [~,vertices1,vertices2] = twolink_kinematicMap(theta(:,i));
         twolink_plot(theta(:,i),'r',[0;0])
-        flagTheta = any([any(polygon_isCollision(vertices1,points)) any(polygon_isCollision(vertices2,points))]);
+        flagTheta(1,i) = any([any(polygon_isCollision(vertices1,points)) any(polygon_isCollision(vertices2,points))]);
 end
 %For this question,  do not consider self-collision (i.e., if the two polygons
 %overlap but they do not cover any of the points, then it is not a collision).
